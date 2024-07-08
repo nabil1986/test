0a77c4e95df12814bfd5a8423b239968bb60728e
nabil1986 <kaciminabil@gmail.com>
2024-07-08 17:58:20 +0100

    Initial commit

---
+++ models/Thing.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const thingSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  inStock: { type: Boolean },
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
